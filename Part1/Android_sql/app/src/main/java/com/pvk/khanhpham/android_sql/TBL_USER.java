e8e49457b5e1f55998aaf74409fab0eddff59fc3
phamvietkhanh9 <khanhpvth1808010@fpt.edu.vn>
2020-09-07 16:39:37 +0700

    part1

---
+++ Part1/Android_sql/app/src/main/java/com/pvk/khanhpham/android_sql/TBL_USER.java
@@ -0,0 +1,8 @@
+package com.pvk.khanhpham.android_sql;
+
+public class TBL_USER {
+    public static String id="id";
+    public static String name="name";
+    public static String gender ="gender";
+    public static String age ="age";
+}
